939fe7a52b8735f19653cad462d5fec3df1d477c
Kevin Keller <flygeneticist@gmail.com>
2017-02-06 17:07:02 +0800

    more additons to script file

--- migrate.py
+++ migrate.py
@@ -1,1 +1,29 @@
 import requests
+import json 
+
+Helium Comedy Club  - 1
+
+Helium Comedy Club Portland - 5
+
+Helium Portland Store - 6
+
+Helium Philly Store - 7
+
+Helium & Elements Restaurant - 21
+
+Helium Buffalo Store - 23
+
+Goodnights & Factory Restaurant - 53
+
+Goodnights Store - 63
+
+Levé's Annual Charity Ball Sponsoring New Avenues For Youth  - 131
+
+Helium & Elements Restaurant - 133
+
+venue_ids = []
+show_id = ""
+user_id = ""
+
+get_shows_url = "/api/venues/%s/shows" % venue_id
+get_willcall_url = "seatengine.com/api/venues/%s/shows/%s/willcall" % (venue_id, show_id)
