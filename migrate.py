82960bd84365c35a9c3d75b4a61dc5088895384e
Kevin Keller <flygeneticist@gmail.com>
2017-02-06 19:12:42 +0800

    processing order logic edited for missing data values

--- migrate.py
+++ migrate.py
@@ -20,17 +20,27 @@ def get_show_orders(venue_id, show_id, header):
     data = json.loads(res.text)
     order_info = []
     for order in data:
-        order_info.append({
+        temp = {
             'id': order['customer']['id'],
             'first_name': order['customer']['first_name'],
             'last_name': order['customer']['last_name'],
             'email': order['customer']['email'],
             'new_customer': order['customer']['new_customer'],
-            'payment_method': order['payments'][0]['payment_method'],
             'order_number': order['order_number'],
-            'price': order['price'],
-            'purchase_at': order['purchase_at']
-        })
+            'purchase_date': order['purchase_at'],
+            'total_sold': 0,
+            'total_price': 0
+        }
+        for tix_type in order['tickets']:
+            counter = 0
+            for tix in tix_type:
+                try:
+                    temp['total_sold'] += 1
+                    temp['total_price'] += order['tickets'][tix_type][counter]['price']
+                except:
+                    pass
+                counter += 1
+        order_info.append(temp)
     return order_info
 
 
@@ -42,6 +52,7 @@ def main():
         salesforce_data[venue] = get_venue_shows(venue, auth_header)
         for show in salesforce_data[venue]:
             salesforce_data[venue][show] = get_show_orders(venue, show, auth_header)
+    print salesforce_data
     return salesforce_data
 
 if __name__ == '__main__':
