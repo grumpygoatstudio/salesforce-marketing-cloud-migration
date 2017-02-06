9a4f873b3d82a91ac2dba9c115aa2d8bf6ec434b
Kevin Keller <flygeneticist@gmail.com>
2017-02-06 18:47:26 +0800

    more work on script json parsing logic for order info data

--- migrate.py
+++ migrate.py
@@ -7,23 +7,42 @@ def load_config():
         return json.load(f)
 
 
-def get_venue_shows(venue_id):
-    url = "/api/venues/%s/shows" % venue_id
-    res = requests(url, header=auth)
-    return json.dumps(res)
+def get_venue_shows(venue_id, header):
+    url = "https://api-2.seatengine.com/api/venues/%s/shows" % venue_id
+    res = requests.get(url, headers=header)
+    data = json.loads(res.text)['data']
+    return {show['event']['id']:[] for show in data}
 
 
-def get_show_orders(venue_id, show_id):
-    url = "seatengine.com/api/venues/%s/shows/%s/willcall" % (venue_id, show_id)
+def get_show_orders(venue_id, show_id, header):
+    url = "https://api-2.seatengine.com/api/venues/%s/shows/%s/willcall" % (venue_id, show_id)
+    res = requests.get(url, headers=header)
+    data = json.loads(res.text)
+    order_info = []
+    for order in data:
+        order_info.append({
+            'id': order['customer']['id'],
+            'first_name': order['customer']['first_name'],
+            'last_name': order['customer']['last_name'],
+            'email': order['customer']['email'],
+            'new_customer': order['customer']['new_customer'],
+            'payment_method': order['payments'][0]['payment_method'],
+            'order_number': order['order_number'],
+            'price': order['price'],
+            'purchase_at': order['purchase_at']
+        })
+    return order_info
 
 
 def main():
-    pass
-
-
-venue_ids = [1, 5, 6, 7, 21, 23, 53, 63, 131, 133]
-auth = load_config()
-print auth['X-User-Token']
+    venues = [1] #, 5, 6, 7, 21, 23, 53, 63, 131, 133]
+    auth_header = load_config()    
+    salesforce_data = {}
+    for venue in venues:
+        salesforce_data[venue] = get_venue_shows(venue, auth_header)
+        for show in salesforce_data[venue]:
+            salesforce_data[venue][show] = get_show_orders(venue, show, auth_header)
+    return salesforce_data
 
 if __name__ == '__main__':
     main()
