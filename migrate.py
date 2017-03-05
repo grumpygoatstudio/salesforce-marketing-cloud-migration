58952f629f01ba1b958c2e0f735deee5646a3484
Kevin Keller <flygeneticist@gmail.com>
2017-03-05 21:48:18 +0800

    updates to date processing from SeatEngine API

--- migrate.py
+++ migrate.py
@@ -10,50 +10,58 @@ def load_config():
 def get_venue_shows(venue_id, header):
     url = "https://api-2.seatengine.com/api/venues/%s/shows" % venue_id
     res = requests.get(url, headers=header)
-    data = json.loads(res.text)['data']
-    return {show['event']['id']:[] for show in data}
-
+    if res.status_code == 200:
+        data = json.loads(res.text)['data']
+        print data
+        return {show['event']['id']:[] for show in data}
+    else:
+        return False
 
 def get_show_orders(venue_id, show_id, header):
     url = "https://api-2.seatengine.com/api/venues/%s/shows/%s/willcall" % (venue_id, show_id)
     res = requests.get(url, headers=header)
-    data = json.loads(res.text)
+    if res.status_code == 200:
+        data = json.loads(res.text)
+        return data
+    else:
+        return False
+
+
+def create_ticketevents_from_orders(orders):
     order_info = []
-    for order in data:
+    for order in orders:
         temp = {
-            'id': order['customer']['id'],
-            'first_name': order['customer']['first_name'],
-            'last_name': order['customer']['last_name'],
-            'email': order['customer']['email'],
-            'new_customer': order['customer']['new_customer'],
             'order_number': order['order_number'],
             'purchase_date': order['purchase_at'],
-            'total_sold': 0,
-            'total_price': 0
+            'booking_type': order['booking_type'],
+            'new_customer': order['customer']['new_customer'],
+            'cust_id': order['customer']['id'],
+            'name': order['customer']['name'],
+            'email': order['customer']['email'],
+            'order_total': 0,
+            'tickets': {}
         }
         for tix_type in order['tickets']:
-            counter = 0
-            for tix in tix_type:
-                try:
-                    temp['total_sold'] += 1
-                    temp['total_price'] += order['tickets'][tix_type][counter]['price']
-                except:
-                    pass
-                counter += 1
+            prices = [tix['price'] for tix in order['tickets'][tix_type]]
+            temp['tickets'][tix_type] = prices
+            temp['order_total'] += sum(prices)
+            # POST NEW ORDERLINE FOR TICKET TYPE
         order_info.append(temp)
     return order_info
 
 
 def main():
-    venues = [1] #, 5, 6, 7, 21, 23, 53, 63, 131, 133]
+    venues = [5] #[1, 5, 6, 7, 21, 23, 53, 63, 131, 133]
     auth_header = load_config()    
     salesforce_data = {}
     for venue in venues:
-        salesforce_data[venue] = get_venue_shows(venue, auth_header)
+        shows = get_venue_shows(venue, auth_header)
+        salesforce_data[venue] = shows if shows else []
         for show in salesforce_data[venue]:
-            salesforce_data[venue][show] = get_show_orders(venue, show, auth_header)
+            orders = get_show_orders(venue, show, auth_header)
+            salesforce_data[venue][show] = create_ticketevents_from_orders(orders) if orders else []
     print salesforce_data
-    return salesforce_data
+
 
 if __name__ == '__main__':
     main()
