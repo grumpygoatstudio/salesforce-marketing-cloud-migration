f065e2203c558d9f1ce13c6d7d81f6f8f636e354
Kevin Keller <flygeneticist@gmail.com>
2018-04-26 20:36:51 +0700

    overhaul to data and table setup

--- migrate.py
+++ migrate.py
@@ -22,17 +22,25 @@ def write_config(config, dir_path):
         json.dump(config, f)
 
 
-def get_venue_shows(venue_id, pull_limit, header):
+def get_venue_events_and_shows(venue_id, pull_limit, header):
     url = "https://api-2.seatengine.com/api/venues/%s/events" % venue_id
     res = requests.get(url, headers=header)
     if res.status_code == 200:
-        events = json.loads(res.text)['data']
+        data = json.loads(res.text)['data']
+        events = []
         shows = []
-        for event in events:
+        for event in data:
+            # build event objects
+            events.append({
+                'id': event['id'],
+                'venue_id': str(venue_id),
+                'name': event['name'].strip().replace("\"", "").replace(",", " "),
+                'logo_url': event['image_url'],
+            })
             for show in event['shows']:
                 if parse(show['start_date_time'], ignoretz=True) > pull_limit:
                     shows.append(show['id'])
-        return shows
+        return (events, shows)
     else:
         return []
 
@@ -42,15 +50,13 @@ def get_show_information(venue_id, show_id, header):
     res = requests.get(url, headers=header)
     if res.status_code == 200:
         show = json.loads(res.text)['data']
-        # build out events and tickets objects
-        event = {}
-        event['id'] = str(show_id)
-        event['venue_id'] = str(venue_id)
-        event['name'] = str(show['event']['name']).strip().replace(",", " "),
-        event['cancelled_at'] = str(show['event']["cancelled_at"])
-        event['start_date_time'] =  str(show['start_date_time'])
-        event["sold_out"] = show["sold_out"]
-        return event
+        return {
+            'id': str(show_id),
+            'event_id': show['event_id'],
+            'start_date_time': str(show['start_date_time']),
+            'sold_out': show["sold_out"],
+            'cancelled_at': str(show['event']["cancelled_at"])
+        }
     else:
         return False
 
@@ -90,10 +96,13 @@ def create_objects_from_orders(orders, event_id, pull_limit):
                 'order_number': str(order['order_number']),
                 'cust_id': str(order['customer']['id']),
                 'email': str(order['customer']['email']),
+                'phone': str(order['customer']['phone']),
                 'purchase_date': str(order['purchase_at']),
                 'payment_method': payment_method,
                 'booking_type': str(order['booking_type']),
                 'order_total': 0,
+                'new_customer': str(order['customer']['new_customer']),
+                'addons': ", ".join([str(a) for a in order['addons']])
             }
 
             for tix_type in order['tickets']:
@@ -101,16 +110,17 @@ def create_objects_from_orders(orders, event_id, pull_limit):
                 # add ticket costs to ORDER total
                 temp_order['order_total'] += sum(prices)
                 # add tickets purchased to ORDERLINE
-                temp_orderline = {
-                    'id': str(order['order_number']) + "-%0.6d" % random.randint(0, 999999),
-                    'order_number': str(order['order_number']),
-                    'line_subtotal': sum(prices),
-                    'ticket_price': prices[0],
-                    'quantity': len(prices),
-                    'ticket_name': str(tix_type).strip().replace(",", " "),
-                    'event_id': str(event_id)
-                }
-                orderlines_info += [temp_orderline]
+                for tix in order['tickets'][tix_type]:
+                    temp_orderline = {
+                        'id': str(order['order_number']) + "-%0.6d" % random.randint(0, 999999),
+                        'order_number': str(order['order_number']),
+                        'ticket_name': str(tix_type).strip().replace(",", " "),
+                        'ticket_price': tix['price'],
+                        'printed': tix['printed'],
+                        'promo_code_id': tix['promo_code_id'],
+                        'checked_in': tix['checked_in'],
+                    }
+                    orderlines_info += [temp_orderline]
 
             orders_info += [temp_order]
             customers_info += [temp_cust]
@@ -127,21 +137,28 @@ def main():
         pull_limit = datetime.today()
     else:
         pull_limit = parse(configs['last_pull'], ignoretz=True)
-    data = {
-        "venues": [1, 5, 6, 7, 21, 23, 53, 63, 131, 133, 297],
-        "events": [],
-        "orderlines": [],
-        "orders": [],
-        "contacts": []
-    }
 
-    # collect and process all data from API source
-    for venue_id in data['venues']:
+    venues = [1, 5, 6, 7, 21, 23, 53, 63, 131, 133, 297]
+    data_types = ["events", "shows", "orderlines", "orders", "contacts"]
+
+    # COLLECT AND PROCESS ALL DATA FROM API SOURCE
+    for venue_id in venues:
+        data = {
+            "events": [],
+            "shows": [],
+            "orderlines": [],
+            "orders": [],
+            "contacts": []
+        }
         print("Processing venue: " + str(venue_id))
-        for show_id in get_venue_shows(venue_id, pull_limit, auth_header):
+        events_and_shows = get_venue_events_and_shows(venue_id, pull_limit, auth_header)
+        events = events_and_shows[0]
+        shows = events_and_shows[1]
+
+        for show_id in shows:
             show_info = get_show_information(venue_id, show_id, auth_header)
             if show_info:
-                data['events'] += [show_info]
+                data['shows'] += [show_info]
             show_orders = get_show_orders(venue_id, show_id, auth_header)
             if show_orders:
                 order_info_objs = create_objects_from_orders(show_orders, show_id, pull_limit)
@@ -149,15 +166,14 @@ def main():
                 data['orderlines'] += order_info_objs[1]
                 data['contacts'] += order_info_objs[2]
 
-    for dt in data:
-        if dt != 'venues':
+        for dt in data_types:
             try:
-                file_path = os.path.join(dir_path, 'api-data', dt + '.csv')
-                # remove old CSV file from filesystem if it exists
+                file_path = os.path.join(dir_path, 'api-data', dt + '-' + str(venue_id) + '.csv')
                 os.remove(file_path)
             except OSError:
                 pass
-            # build csv files from the API source data collected
+
+            # BUILD CSV FILES FROM THE API SOURCE DATA COLLECTED
             the_file = open(file_path, "w")
             if len(data[dt]) > 0:
                 writer = DictWriter(the_file, data[dt][0].keys())
@@ -165,9 +181,10 @@ def main():
                 writer.writerows(data[dt])
             the_file.close()
 
-    for dt in data:
-        if dt != 'venues':
-            file_path = os.path.join(dir_path, 'api-data', dt + '.csv')
+    # UPLOAD ALL SQL FILES TO AWS RDS SERVER
+    for venue_id in venues:
+        for dt in data_types:
+            file_path = os.path.join(dir_path, 'api-data', dt + '-' + str(venue_id) + '.csv')
             # upload new CSV file to the MySQL DB
             sql_cmd = """mysql %s -h %s -P %s -u %s --password=%s -e \"LOAD DATA LOCAL INFILE '%s' INTO TABLE %s FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\\"' IGNORE 1 LINES; SHOW WARNINGS\"""" % (
                 configs['db_name'],
@@ -180,7 +197,7 @@ def main():
             )
             os.system(sql_cmd)
 
-    # write new datetime for last pulled time
+    # WRITE NEW DATETIME FOR LAST PULLED TIME
     configs['last_pull'] = datetime.today().strftime("%Y-%m-%dT%H:%M:%S")
     write_config(configs, dir_path)
 
