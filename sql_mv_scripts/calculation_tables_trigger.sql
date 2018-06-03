0669650af6d76d0d66dda1643f9ab345a45565c7
Kevin Keller <flygeneticist@gmail.com>
2018-06-03 14:25:34 +0700

    changes to triggers

---
+++ sql_mv_scripts/calculation_tables_trigger.sql
@@ -0,0 +1,208 @@
+DROP PROCEDURE refresh_calculation_tables_now;
+
+DELIMITER $$
+
+CREATE PROCEDURE refresh_calculation_tables_now (
+    OUT rc INT
+)
+BEGIN
+		# turn OFF all FK constraints for tables 
+        SET FOREIGN_KEY_CHECKS = 0; 
+        
+		# truncate all tables
+        TRUNCATE TABLE attended_event_dates;
+        TRUNCATE TABLE avg_tickets_per_order;
+        TRUNCATE TABLE fln_event_data;
+        TRUNCATE TABLE last_360;
+        TRUNCATE TABLE per_paid_order;
+        TRUNCATE TABLE per_comp_order;
+        TRUNCATE TABLE special_shows;
+        TRUNCATE TABLE weekday_attendence;
+        
+		# Table for pulling all the first/last/next dates from
+        INSERT INTO attended_event_dates
+			SELECT c.subscriber_key AS subscriber_key,
+				start_date_formatted,
+				e.name AS event_title,
+				v.id AS event_venue
+			FROM seatengine.contacts c
+			JOIN seatengine.orders_processed o ON (o.cust_id = c.subscriber_key)
+			JOIN seatengine.shows_processed s ON (s.id = o.show_id)
+			JOIN seatengine.events_processed e ON (e.id = s.event_id)
+			JOIN seatengine.venues_processed v ON (v.id = e.venue_id)
+		;
+        
+        # table for joining in all the first/last/next dates
+        INSERT INTO fln_event_data
+			SELECT c.subscriber_key,
+				first_show_attended, -- First Show Attended Date
+				first_event_title, -- First Event Title
+				first_event_venue, -- First Event Venue
+				last_show_attended, -- Last Show Attended Date
+				last_event_title, -- Last Event Title
+				last_event_venue, -- Last Event Venue
+				next_show_attending, -- Next Show Attending Date
+				next_event_title -- Next Event Title	
+			FROM seatengine.contacts c
+			## FIRST SHOW / EVENT INFO PER CUSTOMER
+			LEFT JOIN
+				(SELECT a1.subscriber_key AS subscriber_key,
+					MIN(start_date_formatted) AS first_show_attended, -- First Show Attended Date
+					event_title AS first_event_title, -- First Event Title
+					event_venue AS first_event_venue -- First Event Venue
+				FROM attended_event_dates a1
+				WHERE start_date_formatted <= NOW() 
+				) AS a1 ON (c.subscriber_key = a1.subscriber_key)
+			## LAST SHOW / EVENT INFO PER CUSTOMER
+			LEFT JOIN 
+				(SELECT subscriber_key,
+					MAX(start_date_formatted) AS last_show_attended, -- Last Show Attended Date
+					event_title AS last_event_title, -- Last Event Title
+					event_venue AS last_event_venue -- Last Event Venue
+				FROM attended_event_dates a2
+				WHERE start_date_formatted <= NOW()
+				GROUP BY subscriber_key
+				) AS a2 ON (c.subscriber_key = a2.subscriber_key)
+			## NEXT SHOW / EVENT INFO PER CUSTOMER
+			LEFT JOIN
+				(SELECT subscriber_key,
+					MIN(start_date_formatted) AS next_show_attending, -- Next Show Attending Date
+					event_title AS next_event_title -- Next Event Title
+				FROM attended_event_dates a3
+				WHERE start_date_formatted > NOW() 
+				GROUP BY subscriber_key
+				) AS a3 ON (c.subscriber_key = a3.subscriber_key)
+		;
+        
+        
+        INSERT INTO avg_tickets_per_order
+			SELECT c.subscriber_key,
+				AVG(DATEDIFF(start_date_formatted,purchase_date_formatted)) avg_purchase_to_show_days, -- AVG Number of days between purchase and show
+				SUM(ticket_counts.ticket_count) / COUNT(o.order_number) as avg_tickets_per_order -- Average Number of Lifetime Tickets Per Order
+			FROM seatengine.contacts c
+			JOIN seatengine.orders_processed o ON (o.cust_id = c.subscriber_key)
+			JOIN seatengine.shows_processed s ON (o.show_id = s.id)
+			JOIN (
+					SELECT omv.orderNumber, COUNT(omv.unique_id) ticket_count
+					FROM seatengine.orders_mv omv
+					GROUP BY omv.orderNumber
+			) ticket_counts ON (ticket_counts.orderNumber = o.order_number)
+			GROUP BY c.subscriber_key
+		;
+        
+        ## TICKET / ORDER STATS FOR LIFETIME PAID ORDERS
+        INSERT INTO per_paid_order
+			SELECT c.subscriber_key,
+				COUNT(o.order_number) total_lifetime_paid_orders, -- Total Lifetime Paid Orders
+				SUM(ticket_counts.ticket_count) total_lifetime_paid_tickets, -- Total Number of Lifetime Paid Tickets
+				SUM(ticket_counts.ticket_count) /  COUNT(o.order_number) avg_tickets_per_paid_order
+			FROM seatengine.contacts c
+			JOIN seatengine.orders_processed o ON (o.cust_id = c.subscriber_key)
+			JOIN (
+					SELECT omv.orderNumber, count(omv.unique_id) ticket_count
+					FROM seatengine.orders_mv omv
+					WHERE omv.not_comped = 1
+					GROUP BY omv.orderNumber
+			) ticket_counts ON (ticket_counts.orderNumber = o.order_number)
+			GROUP BY c.subscriber_key
+		;
+
+		## TICKET / ORDER STATS FOR LIFETIME COMP'D ORDERS
+		## COMPUTE LAST COMP'D SHOW DATE PER CUSTOMER
+        INSERT INTO per_comp_order
+			SELECT c.subscriber_key,
+				MAX(start_date_formatted) AS last_comp_show_date, -- Last Comp Show Date
+				COUNT(o.order_number) total_lifetime_comp_orders, -- Total Lifetime Comp'd Orders
+				SUM(ticket_counts.ticket_count) total_lifetime_comp_tickets, -- Total Lifetime Comp'd Tickets
+				SUM(ticket_counts.ticket_count) /  COUNT(o.order_number) avg_tickets_per_comp_order
+			FROM seatengine.contacts c
+			JOIN seatengine.orders_processed o ON (o.cust_id = c.subscriber_key)
+			JOIN seatengine.shows_processed s ON (o.show_id = s.id)
+			JOIN (
+					SELECT omv.orderNumber, count(omv.unique_id) ticket_count
+					FROM seatengine.orders_mv omv
+					WHERE omv.comped = 1
+					GROUP BY omv.orderNumber
+			) ticket_counts ON (ticket_counts.orderNumber = o.order_number)
+			GROUP BY c.subscriber_key
+        ;
+        
+        ## EVENT ATTENDANCE COUNTS BY WEEKDAY PER CUSTOMER
+        INSERT INTO weekday_attendence
+			SELECT wk.cust_id AS subscriber_key,
+					COUNT(CASE WHEN show_day = 0 THEN 1 END) AS shows_attended_M, -- Comp'd or Paid Events Attended on Monday
+					COUNT(CASE WHEN show_day = 1 THEN 1 END) AS shows_attended_T, -- Comp'd or Paid Events Attended on Tuesday
+					COUNT(CASE WHEN show_day = 2 THEN 1 END) AS shows_attended_W, -- Comp'd or Paid Events Attended on Wednesday
+					COUNT(CASE WHEN show_day = 3 THEN 1 END) AS shows_attended_R, -- Comp'd or Paid Events Attended on Thursday
+					COUNT(CASE WHEN show_day = 4 THEN 1 END) AS shows_attended_F, -- Comp'd or Paid Events Attended on Friday
+					COUNT(CASE WHEN show_day = 5 THEN 1 END) AS shows_attended_S, -- Comp'd or Paid Events Attended on Saturday
+					COUNT(CASE WHEN show_day = 6 THEN 1 END) AS shows_attended_U -- Comp'd or Paid Events Attended on Sunday
+				FROM (
+						SELECT o.cust_id AS cust_id,
+							s.start_date_formatted AS show_day,
+							COUNT(*) AS shows_attended
+						FROM seatengine.orders_processed o
+						JOIN seatengine.shows_processed s ON (o.show_id = s.id)
+						GROUP BY o.cust_id, show_day
+					) AS wk
+				GROUP BY subscriber_key
+		;
+
+		## COUNT ORDERS BY PAYMENT TYPE & PAID ORDER REVENE IN LAST 90,180,360 DAYS PER CUSTOMER
+        INSERT INTO last_360
+			SELECT
+					o.cust_id AS subscriber_key,
+					SUM(CASE WHEN o.not_comped = 1
+									AND purchase_date_formatted BETWEEN NOW() - INTERVAL 360 DAY AND NOW()
+									THEN o.order_total END) AS paid_orders_revenue_360,
+					SUM(CASE WHEN o.not_comped = 1
+									AND purchase_date_formatted BETWEEN NOW() - INTERVAL 180 DAY AND NOW()
+									THEN o.order_total END) AS paid_orders_revenue_180,
+					SUM(CASE WHEN o.not_comped = 1
+									AND purchase_date_formatted BETWEEN NOW() - INTERVAL 90 DAY AND NOW()
+									THEN o.order_total END) AS paid_orders_revenue_90,
+					COUNT(CASE WHEN o.not_comped = 1
+									AND purchase_date_formatted BETWEEN NOW() - INTERVAL 360 DAY AND NOW()
+									THEN 1 END) AS paid_orders_count_360,
+					COUNT(CASE WHEN o.not_comped = 1
+								AND purchase_date_formatted BETWEEN NOW() - INTERVAL 180 DAY AND NOW()
+								THEN 1 END) AS paid_orders_count_180,
+					COUNT(CASE WHEN o.not_comped = 1
+									AND purchase_date_formatted BETWEEN NOW() - INTERVAL 90 DAY AND NOW()
+									THEN 1 END) AS paid_orders_count_90,
+					COUNT(CASE WHEN o.comped = 1
+									AND purchase_date_formatted BETWEEN NOW() - INTERVAL 360 DAY AND NOW()
+									THEN 1 END) AS comp_orders_count_360,
+					COUNT(CASE WHEN o.comped = 1
+									AND purchase_date_formatted BETWEEN NOW() - INTERVAL 180 DAY AND NOW()
+									THEN 1 END) AS comp_orders_count_180,
+					COUNT(CASE WHEN o.comped = 1
+									AND purchase_date_formatted BETWEEN NOW() - INTERVAL 90 DAY AND NOW()
+									THEN 1 END) AS comp_orders_count_90
+				FROM seatengine.orders_processed o
+				GROUP BY subscriber_key
+		;
+        
+        ## COUNT EVENTS THAT ARE SPECIAL TYPES PER CUSTOMER
+        INSERT INTO special_shows
+			SELECT
+				o.cust_id AS subscriber_key,
+                o.phone AS phone,
+                SUM(o.order_total) AS total_revenue, -- Total Revenue from customerid
+				MAX(o.purchase_date_formatted) AS last_ordered_date, -- Last Ordered Date
+				SUM(e.special_event) AS count_shows_special, -- Special Event Total Order Count
+				SUM(e.presents_event) AS count_shows_persents -- Presents Event Total Order Count
+			FROM seatengine.orders_processed o
+			JOIN seatengine.shows_processed s ON (s.id = o.show_id)
+			JOIN seatengine.events_processed e ON (e.id = s.event_id)
+			GROUP BY subscriber_key
+		;
+        
+        # turn ON all FK constraints for tables 
+        SET FOREIGN_KEY_CHECKS = 1; 
+
+  SET rc = 0;
+END;
+$$
+
+DELIMITER ;
--- sql_mv_scripts/processed_tables_trigger.sql
+++ sql_mv_scripts/processed_tables_trigger.sql
@@ -17,12 +17,31 @@ BEGIN
 
         # update orderlines 
         INSERT INTO orderlines_processed
-		SELECT ol.*
+		SELECT 
+			ol.id AS id,
+            ol.order_number AS order_number,
+            ol.ticket_name AS ticket_name,
+            ol.ticket_price AS ticket_price,
+            ol.printed AS printed,
+            ol.promo_code_id AS promo_code_id,
+            ol.checked_in AS checked_in
 		FROM seatengine.orderlines ol;
         
 		# update orders 
         INSERT INTO orders_processed
-        SELECT o.*,
+        SELECT 
+			o.id AS id,
+            o.show_id AS show_id,
+            o.order_number AS order_number,
+            o.cust_id AS cust_id,
+            o.email AS email,
+            o.phone AS phone,
+            o.purchase_date AS purchase_date,
+            o.payment_method AS payment_method,
+            o.booking_type AS booking_type,
+            o.order_total AS order_total,
+            o.new_customer AS new_customer,
+            o.addons AS addons,
             DATE_FORMAT(purchase_date, '%Y-%m-%d') AS purchase_date_formatted,
 			CASE WHEN o.payment_method LIKE '%comp%' THEN 1 ELSE 0 END AS comped,
             CASE WHEN o.payment_method NOT LIKE '%comp%' THEN 1 ELSE 0 END AS not_comped
@@ -30,7 +49,12 @@ BEGIN
         
         # update shows
         INSERT INTO shows_processed
-        SELECT s.*,
+        SELECT 
+			s.id AS id,
+            s.event_id AS event_id,
+            s.start_date_time AS start_date_time,
+            s.sold_out AS sold_out,
+            s.cancelled_at AS cancelled_at,
 			DATE_FORMAT(s.start_date_time, '%Y-%m-%d') AS start_date_formatted,
 			WEEKDAY(DATE_FORMAT(s.start_date_time, '%Y-%m-%d')) AS day_of_week
 		FROM seatengine.shows s;
@@ -48,6 +72,11 @@ BEGIN
         
         # turn ON all FK constraints for tables 
         SET FOREIGN_KEY_CHECKS = 1; 
+        
+        # call triggers to update final two MVs
+        CALL refresh_orders_mv_now(@rc);
+        CALL refresh_calculation_tables_now(@rc);
+        CALL refresh_contacts_mv_now(@rc);
 
   SET rc = 0;
 END;
