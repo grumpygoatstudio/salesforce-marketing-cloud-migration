1ec2a2794bd6b0dd78d6e94d108456bc4e8d4bba
Kevin Keller <flygeneticist@gmail.com>
2018-06-09 14:14:31 +0700

    remove extra quote

--- crm_sync.py
+++ crm_sync.py
@@ -76,7 +76,7 @@ def active_campaign_sync():
         except OSError:
             pass
         # download CSV file from MySQL DB
-        sql_cmd = """mysql %s -h %s -P %s -u %s --password=%s -e \"SELECT * FROM orders_mv WHERE venue_id = %s AND (sys_entry_date = '0000-00-00 00:00:00' OR sys_entry_date > \'%s\'');\" > %s""" % (
+        sql_cmd = """mysql %s -h %s -P %s -u %s --password=%s -e \"SELECT * FROM orders_mv WHERE venue_id = %s AND (sys_entry_date = '0000-00-00 00:00:00' OR sys_entry_date > \'%s\');\" > %s""" % (
             configs['db_name'],
             configs['db_host'],
             configs['db_port'],
