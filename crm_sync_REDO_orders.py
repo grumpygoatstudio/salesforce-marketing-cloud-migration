f50d1c462b0076d4301d22afa1f309fad37fb4da
Kevin Keller <flygeneticist@gmail.com>
2018-07-15 07:41:49 +0700

    chunking of dicts

--- crm_sync_REDO_orders.py
+++ crm_sync_REDO_orders.py
@@ -149,6 +149,12 @@ def update_data(url, auth_header, data, configs, connection, obj_type):
         return 'err-unicode'
 
 
+def chunks(data, SIZE=10000):
+    it = iter(data)
+    for i in xrange(0, len(data), SIZE):
+        yield {k: data[k] for k in islice(it, SIZE)}
+
+
 def active_campaign_sync():
     dir_path = os.path.dirname(os.path.abspath(__file__))
     configs = load_config(dir_path)
@@ -157,10 +163,10 @@ def active_campaign_sync():
     last_crm_sync = "00-00-00T00:00:01"
     orders_url = configs['Api-Url'] + 'ecomOrders'
     customers_url = configs['Api-Url'] + 'ecomCustomers'
-    venues = [(5, '4'),  (6, '5'), (133, '8'), (7, '6'),
-              (21, '7'), (23, '10'), (1, '3'), (297, '2')]
+    venues = [(1, '3'), (21, '7')]
 
     for venue_id, connection in venues:
+
         # setup a completion email notifying Kevin and Jason that a Month of Venue pushes has finished
         sender = "kevin@matsongroup.com"
         recipients = ["flygeneticist@gmail.com", "jason@matsongroup.com"]
@@ -193,50 +199,57 @@ def active_campaign_sync():
                 more_rows = False
         db.close()
 
+        cust_count = 0
+        cust_err = {'build':0, 'push':0, 'unicode':0}
+        order_count = 0
+        order_err = {'build': 0, 'update': 0, 'unicode': 0, 'other': 0}
+
         print("~~ POSTING CUSTOMERS ~~")
         print("TOTAL ORDERS TO PUSH: %s" % len(orders))
         crm_postings = []
-        cust_err = {'build':0, 'push':0, 'unicode':0}
-        for o in orders:
-            ols = orders[o]
-            # build order and customer JSON and POST the JSON objects to AC server
-            customer_json = build_customer_json(connection, ols)
-            if customer_json:
-                crm_id = update_data(
-                    customers_url, auth_header, customer_json, configs, connection, 'ecomCustomer')
-                if crm_id == 'err-unicode':
-                    cust_err["unicode"] += 1
-                elif crm_id:
-                    crm_postings.append([crm_id, ols])
+        for chunk in chunks(orders, 10000):
+            for o in chunk:
+                ols = chunk[o]
+                # build order and customer JSON and POST the JSON objects to AC server
+                customer_json = build_customer_json(connection, ols)
+                if customer_json:
+                    crm_id = update_data(
+                        customers_url, auth_header, customer_json, configs, connection, 'ecomCustomer')
+                    if crm_id == 'err-unicode':
+                        cust_err["unicode"] += 1
+                    elif crm_id:
+                        crm_postings.append([crm_id, ols])
+                    else:
+                        cust_err['push'] += 1
                 else:
-                    cust_err['push'] += 1
-            else:
-                print("BUILD CUSTOMER JSON FAILED!", str(ols))
-                cust_err['build'] += 1
+                    print("BUILD CUSTOMER JSON FAILED!", str(ols))
+                    cust_err['build'] += 1
 
-        print("~~ POSTING ORDERS PAYLOAD ~~")
-        print("TOTAL ORDERS TO PUSH: %s" % len(crm_postings))
-        order_count = 0
-        order_err = {'build':0, 'update':0, 'unicode':0, 'other':0}
-        for i in crm_postings:
-            try:
-                crm_order = build_order_json(connection, str(i[0]), i[1])
-                updated = update_data(orders_url, auth_header, crm_order, configs, connection, 'ecomOrder')
-                if updated == "success":
-                    order_count += 1
-                elif updated == 'err-update':
-                    order_err["update"] += 1
-                elif updated == 'err-unicode':
-                    order_err["unicode"] += 1
-                elif updated == 'err-other':
-                    order_err["other"] += 1
-            except:
-                print("BUILD ORDER JSON FAILED!", str(i[1][0]["orders_mv.orderNumber"]))
-                order_err['build'] += 1
+            print("~~ POSTING ORDERS PAYLOAD ~~")
+            print("TOTAL ORDERS TO PUSH: %s" % len(crm_postings))
+            cust_success += len(crm_postings)
+            for i in crm_postings:
+                try:
+                    crm_order = build_order_json(connection, str(i[0]), i[1])
+                    updated = update_data(orders_url, auth_header, crm_order, configs, connection, 'ecomOrder')
+                    if updated == "success":
+                        order_count += 1
+                    elif updated == 'err-update':
+                        order_err["update"] += 1
+                    elif updated == 'err-unicode':
+                        order_err["unicode"] += 1
+                    elif updated == 'err-other':
+                        order_err["other"] += 1
+                except:
+                    print("BUILD ORDER JSON FAILED!", str(i[1][0]["orders_mv.orderNumber"]))
+                    order_err['build'] += 1
+            print("Done a chunk! Sleeping for 30 min to avoid SSL issues...")
+            sleep(1800)
+
         # add venue details for the month running to the final email msg
         msg += "~~~~~ VENUE #%s ~~~~~\nCustomers pushed\nSUCCESS Qty: %s\nERROR Qty:\nBuild: %s\nPush: %s\nUnicode: %s\n\nOrders pushed\nSUCCESS Qty: %s\nERRORS Qty:\nBuild: %s\nUpdate: %s\nUnicode: %s\nOther: %s\n" % (
-            venue_id, len(crm_postings), cust_err['build'], cust_err['push'], cust_err['unicode'], order_count, order_err['build'], order_err['update'], order_err['unicode'], order_err['other'])
-        msg += "\nSleeping for 2 hours to avoid SSL issues..."
+            venue_id, cust_count, cust_err['build'], cust_err['push'], cust_err['unicode'], order_count, order_err['build'], order_err['update'], order_err['unicode'], order_err['other'])
+        msg += "\nSleeping for 1 hour to avoid SSL issues..."
         # send a completion email notifying Kevin and Jason that daily updates have finished
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.ehlo()
@@ -244,8 +257,8 @@ def active_campaign_sync():
         server.login(sender, "tie3Quoo!jaeneix2wah5chahchai%bi")
         server.sendmail(sender,recipients, msg)
         server.quit()
-        print("Done! Sleeping for 2 hours to avoid SSL issues..."")
-        sleep(7200)
+        print("Done! Sleeping for 1 hour to avoid SSL issues...")
+        sleep(3600)
 
 if __name__ == '__main__':
     active_campaign_sync()
