65b74e8ee58df6dcc46df13d69f12ac754988b12
Kevin Keller <flygeneticist@gmail.com>
2018-08-14 22:04:44 +0700

    contacts api script checks

--- crm_sync_REDO_contacts.py
+++ crm_sync_REDO_contacts.py
@@ -23,18 +23,62 @@ def write_config(config, dir_path):
         json.dump(config, f)
 
 
-def build_contact_data(data, api_key):
+def build_contact_data(data, api_key, last_venue, list_mappings):
     d = collections.OrderedDict()
     d["api_key"] = api_key
     d["api_action"] = "contact_edit"
     d["api_output"] = "json"
-    d["email"] = str(data['email']).replace("\r", "")
+    d["email"] = str(data['contacts_mv.email_address']).replace("\r", "").strip()
     d["id"] = None
     d["overwrite"] = "0"
-    d["first_name"] = str(data['fname'])
-    d["last_name"] = str(data['lname'])
-    if data['list'] not in ["None", ""]:
-        d["p[%s]" % data['list']] = data['list']
+    d["first_name"] = str(data['contacts_mv.cust_name_first'])
+    d["last_name"] = str(data['contacts_mv.cust_name_last'])
+    # all custom contacts fields
+    d["field[%SEAT_ENGINE_NAME%,0]"] = str(data['contacts_mv.cust_name'])
+    d["field[%SEAT_ENGINE_PHONE%,0]"] = str(data['contacts_mv.phone'])
+    d["field[%SE_TOTAL_ORDERS%,0]"] = str(data['contacts_mv.total_orders'])
+    d["field[%NUMBER_OF_SHOWS_ATTENDED_ON_MONDAYS%,0]"] = str(data['contacts_mv.shows_attended_M'])
+    d["field[%NUMBER_OF_SHOWS_ATTENDED_ON_TUESDAYS%,0]"] = str(data['contacts_mv.shows_attended_T'])
+    d["field[%NUMBER_OF_SHOWS_ATTENDED_ON_WEDNESDAYS%,0]"] = str(data['contacts_mv.shows_attended_W'])
+    d["field[%NUMBER_OF_SHOWS_ATTENDED_ON_THURSDAYS%,0]"] = str(data['contacts_mv.shows_attended_R'])
+    d["field[%NUMBER_OF_SHOWS_ATTENDED_ON_FRIDAYS%,0]"] = str(data['contacts_mv.shows_attended_F'])
+    d["field[%NUMBER_OF_SHOWS_ATTENDED_ON_SATURDAYS%,0]"] = str(data['contacts_mv.shows_attended_S'])
+    d["field[%NUMBER_OF_SHOWS_ATTENDED_ON_SUNDAYS%,0]"] = str(data['contacts_mv.shows_attended_U'])
+    d["field[%FIRST_SHOW_ATTENDED_NAME%,0]"] = str(data['contacts_mv.first_event_title'])
+    d["field[%LAST_SHOW_ATTENDED_NAME%,0]"] = str(data['contacts_mv.last_event_title'])
+    d["field[%NEXT_SHOW_ATTENDING_NAME%,0]"] = str(data['contacts_mv.next_event_title'])
+    first_show = str(data['contacts_mv.first_show_attended']).replace('T', ' ')
+    last_show = str(data['contacts_mv.last_show_attended']).replace('T', ' ')
+    next_show = str(data['contacts_mv.next_show_attending']).replace('T', ' ')
+    last_order = str(data['contacts_mv.last_ordered_date']).replace('T', ' ')
+    last_comp = str(data['contacts_mv.last_comp_show_date']).replace('T', ' ')
+    d["field[%FIRST_SHOW_ATTENDED_DATE%,0]"] = "" if first_show == "None" else first_show
+    d["field[%LAST_SHOW_ATTENDED_DATE%,0]"] = "" if last_show == "None" else last_show
+    d["field[%NEXT_SHOW_ATTENDING_DATE%,0]"] = "" if next_show == "None" else next_show
+    d["field[%LAST_ORDER_DATE%,0]"] = "" if last_order == "None" else last_order
+    d["field[%LAST_COMP_SHOW_DATE%,0]"] = "" if last_comp == "None" else last_comp
+    d["field[%TOTAL_NUMBER_OF_COMP_TICKETS%,0]"] = str(data['contacts_mv.total_lifetime_comp_tickets'])
+    d["field[%TOTAL_NUMBER_OF_COMP_ORDERS%,0]"] = str(data['contacts_mv.total_lifetime_comp_orders'])
+    d["field[%TOTAL_NUMBER_OF_PAID_TICKETS%,0]"] = str(data['contacts_mv.total_lifetime_paid_tickets'])
+    d["field[%TOTAL_NUMBER_OF_PAID_ORDERS%,0]"] = str(data['contacts_mv.total_lifetime_paid_orders'])
+    d["field[%TOTAL_COMP_ORDER_COUNT_LAST_360_DAYS%,0]"] = str(data['contacts_mv.comp_orders_count_360'])
+    d["field[%TOTAL_PAID_ORDER_COUNT_LAST_360_DAYS%,0]"] = str(data['contacts_mv.paid_orders_count_360'])
+    d["field[%TOTAL_REVENUE_LAST_360_DAYS%,0]"] = str(data['contacts_mv.paid_orders_revenue_360'])
+    d["field[%TOTAL_COMP_ORDER_COUNT_LAST_90_DAYS%,0]"] = str(data['contacts_mv.comp_orders_count_90'])
+    d["field[%TOTAL_PAID_ORDER_COUNT_LAST_90_DAYS%,0]"] = str(data['contacts_mv.paid_orders_count_90'])
+    d["field[%TOTAL_REVENUE_LAST_90_DAYS%,0]"] = str(data['contacts_mv.paid_orders_revenue_90'])
+    d["field[%SE_TOTAL_REVENUE%,0]"] = str(data['contacts_mv.total_revenue'])
+    d["field[%COUNT_OF_SPECIAL_EVENTS_ATTENDED%,0]"] = str(data['contacts_mv.count_shows_special'])
+    d["field[%COUNT_OF_PRESENTS_SHOWS_ATTENDED%,0]"] = str(data['contacts_mv.count_shows_persents'])
+    d["field[%AVERAGE_NUMBER_OF_PAID_TICKETS_PER_ORDER%,0]"] = str(data['contacts_mv.avg_tickets_per_paid_order'])
+    d["field[%AVERAGE_NUMBER_OF_TICKETS_PER_COMP_ORDER%,0]"] = str(data['contacts_mv.avg_tickets_per_comp_order'])
+    d["field[%AVERAGE_NUMBER_OF_DAYS_BETWEEN_PURCHASE_AND_EVENT_DATES%,0]"] = str(data['contacts_mv.avg_purchase_to_show_days'])
+    d["field[%AVERAGE_TICKETS_PER_ORDER%,0]"] = str(data['contacts_mv.avg_tickets_per_order'])
+
+    if last_venue not in ["None", ""]:
+        list_id = list_mappings[last_venue]
+        field = "p[%s]" % list_id
+        d[field] = list_id
     return d
 
 
@@ -103,37 +147,60 @@ def active_campaign_sync():
     last_crm_contacts_sync = configs["last_crm_contacts_sync"]
     url = "https://heliumcomedy.api-us1.com/admin/api.php"
     list_mappings = fetch_crm_list_mapping(configs)
-
-    contact_err = 0
+    db = _mysql.connect(user=configs['db_user'],
+                        passwd=configs['db_password'],
+                        port=configs['db_port'],
+                        host=configs['db_host'],
+                        db=configs['db_name'])
+    venue_target = 297
+    db.query("""SELECT  * FROM contacts_mv WHERE email_address != '' AND email_address IN (SELECT DISTINCT email FROM orders_mv WHERE venue_id = \'%s\');"""
+    % venue_target)
+    r = db.store_result()
+    more_rows = True
+    contact_err = {"list":0, "add":0, "update":0, "other":0}
     contact_count = 0
-    file_path = os.path.join(dir_path, 'historic_lists', 'se_cust_truth_Jul6.csv')
-    with open(file_path, "r") as file_data:
-        file_data = csv.reader(file_data, delimiter=',')
-        for l in file_data:
-                contact_info = {
-                    'email': l[0],
-                    'fname': l[1],
-                    'lname': l[2],
-                    'venue': l[3],
-                    'list': l[4],
-                }
-                list_subcrip = str(l[4])
-                contact_data = build_contact_data(contact_info, configs["Api-Token"])
-                if contact_data:
-                    update_contact_in_crm(
-                        url, auth_header, contact_data, configs, list_subcrip)
+
+    while more_rows:
+        try:
+            contact_info = r.fetch_row(how=2)[0]
+            last_venue = str(contact_info['contacts_mv.last_event_venue'])
+            next_venue = str(contact_info['contacts_mv.next_event_venue'])
+            if next_venue != "None":
+                home_venue = next_venue
+            elif last_venue != "None":
+                home_venue = last_venue
+            else:
+                home_venue = ""
+
+            contact_data = build_contact_data(
+                contact_info, configs["Api-Token"], home_venue, list_mappings)
+            if contact_data:
+                updated = update_contact_in_crm(
+                    url, auth_header, contact_data, configs, home_venue)
+                if updated == 'success':
                     contact_count += 1
                 else:
-                    contact_err += 1
-                    print("BUILD CONTACT DATA FAILED!", str(contact_info['email']))
-
+                    if updated == 'err_list':
+                        contact_err['list'] += 1
+                    elif updated == 'err_add':
+                        contact_err['add'] += 1
+                    elif updated == 'err_update':
+                        contact_err['update'] += 1
+                    else:
+                        contact_err['other'] += 1
+            else:
+                contact_err['other'] += 1
+                print("BUILD CONTACT DATA FAILED!", str(
+                    contact_info["contacts_mv.email_address"]))
+        except IndexError:
+            more_rows = False
     # setup a completion email notifying Kevin and Jason that a Month of Venue pushes has finished
     sender = "kevin@matsongroup.com"
     recipients = ["flygeneticist@gmail.com", "jason@matsongroup.com"]
     header = 'From: %s\n' % sender
     header += 'To: %s\n' % ", ".join(recipients)
     header += 'Subject: Completed a MASSIVE UPDATE of Contacts - SeatEngine AWS\n'
-    msg = header + "\nThis is the AWS Server for Seatengine.\nThis is a friendly notice that a push to REDO Contact syncs have completed:\nContacts pushed (SUCCESS qty: %s, ERROR qty: %s)\n" % (contact_count, contact_err)
+    msg = header + "\nThis is the AWS Server for Seatengine.\nThis is a friendly notice that a push to REDO Contact syncs have completed:\n\nTARGET VENUE: %s\n\nContacts pushed (SUCCESS qty: %s, ERROR qty: %s)\n" % (venue_target, contact_count, contact_err)
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
